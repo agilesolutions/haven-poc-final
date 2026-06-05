240ea082a0d1439e089ddd9de0e38f1233de20cb
admin <rong@agile-solutions.ch>
2026-06-05 15:41:20 +0200

    cleaning up tests follow up

--- apps/service-b/src/test/java/com/agilesolutions/service_b/integration/ConcurrentRequestTest.java
+++ apps/service-b/src/test/java/com/agilesolutions/service_b/integration/ConcurrentRequestTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -18,7 +18,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
